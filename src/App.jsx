aafd06457e8ddcb2dbae771b8c71cd4cf682ec03
ChiruChaaya0212 <chirum01004@gmail.com>
2026-01-03 11:13:13 +0530

    importing assests

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger,SplitText } from "gsap/all";
+import Navbar from "./component/Navbar";
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 function App() {
   return (
-    <>
-    <div className="flex-center h-[100vh]">
-     <h1 className="text-3xl text-green-600">HI this my first react page thank you </h1>
-     </div>
-    </>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
